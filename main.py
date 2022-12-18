531611d22509b570bc1e248635ee34de6fa56211
jaskirat-singh-0403 <100020768+jaskirat-singh-0403@users.noreply.github.com>
2022-12-18 14:30:21 +0530

    new files

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import urllib.request
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from io import BytesIO
-from flask import Flask
+from flask import Flask,redirect
 from flask import request
 from flask import render_template
 import numpy as np
@@ -9,11 +9,16 @@ import os
 import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation
-
+import requests
+import json
+import base64
+from summarizer import Summarizer
+from summarizer.sbert import SBertSummarizer
+import numpy as np
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/summarise', methods=['GET','POST'])
 def my_form_post():
     data = request.args
     data =data.get("text")
@@ -22,11 +27,11 @@ def my_form_post():
         return
     zz=data.split("/")[-1]
     zz=zz.split(".")[0]
-    if(not(zz.isalnum( ))):
+    if(not(zz.isidentifier( ))):
             return render_template('output.html', message = "File Name must only contain alphnumeric characters")
     url = "https://www.africau.edu/images/default/sample.pdf"
     writer = PdfFileWriter()
-    remoteFile = urllib.request.urlopen("https://iapportal.herokuapp.com/"+data).read()
+    remoteFile = urllib.request.urlopen("https://iapportal.onrender.com/"+data).read()
     memoryFile = BytesIO(remoteFile)
     pdfFile = PdfFileReader(memoryFile)
     data=""
@@ -34,57 +39,55 @@ def my_form_post():
         currentPage = pdfFile.getPage(pageNum)
         #currentPage.mergePage(watermark.getPage(0))
         data+=currentPage.extractText()
-    stopwords = list(STOP_WORDS)
-    
-    doc1=data
-    nlp =spacy.load('en_core_web_sm')
-    
-    docx = nlp(doc1)
-    
-    mytokens = [token.text for token in docx]
-    
-    word_freq={}
-    for word in docx:
-        if word.text not in stopwords:
-            if word.text not in word_freq.keys():
-                word_freq[word.text]=1;
-                
-            else:
-                word_freq[word.text]+=1;
-                
-    max_freq=max(word_freq.values())
+    bert_model = SBertSummarizer("paraphrase-MiniLM-L6-v2")
+    summary = bert_model(data, num_sentences=10)
+    return redirect('FirstPage.html', message = summary)
 
-    for word in word_freq:
-        word_freq[word]=(word_freq[word]/max_freq)     
-        
-    sentence_list = [ sentence for sentence in docx.sents]
+@app.route('/plag', methods=['GET','POST'])
+def my_form_post1():
+    headers = {
+    'Content-type': 'application/json'
     
-    [w.text.lower() for t in sentence_list for w in t ]
+    }
     
-    
-    sentence_scores={}
-    for sentence in sentence_list:
-        for word in sentence:
-            if word.text in word_freq.keys():
-                if len(sentence.text.split(' ')) < 30:
-                    if sentence not in sentence_scores.keys():
-                        sentence_scores[sentence] = word_freq[word.text]
-                    else:
-                        sentence_scores[sentence] += word_freq[word.text]
-                        
-                        
-    from heapq import nlargest
-    
-    sum_sentences = nlargest(8, sentence_scores, key=sentence_scores.get)
-    
-    final_sentences = [ w.text for w in sum_sentences ] 
-    
-    
-    summary = ' '.join(final_sentences)
-    
-    print(len(doc1))
-    print(len(summary))
-    return render_template('output.html', message = summary)
+    myobj = json.dumps({'email':'jsingh17_be19@thapar.edu','key':'f05dd725-d354-466e-86df-4fc0552f361e'})
+    datas = request.args
+    data =datas.get("text")
+    doc1=datas.get("text")
+    response = requests.post('https://id.copyleaks.com/v3/account/login/api', headers=headers, data=myobj)
+    response=response.json()
+    if len(data)==0:
+        return
+    zz=data.split("/")[-1]
+    zz=zz.split(".")[0]
+    if(not(zz.isidentifier( ))):
+            return render_template('output.html', message = "File Name must only contain alphnumeric characters")
+    url = "https://www.africau.edu/images/default/sample.pdf"
+    writer = PdfFileWriter()
+    #remoteFile = urllib.request.urlopen(url).read()
+    remoteFile = urllib.request.urlopen("https://iapportal.onrender.com/"+data).read()
+    memoryFile = BytesIO(remoteFile)
+    pdfFile = PdfFileReader(memoryFile)
+    data=""
+    for pageNum in range(pdfFile.getNumPages()):
+        currentPage = pdfFile.getPage(pageNum)
+        #currentPage.mergePage(watermark.getPage(0))
+        data+=currentPage.extractText()
+    data=data.encode("utf-8","ignore")
+    dats=base64.b64encode(data)
+    headers = {
+    'Content-type': 'application/json',
+    'Authorization': "bearer "+response["access_token"]
+    }
+    print(str(dats)[1:])
+    print(response["access_token"])
+    print(doc1)
+    url='https://api.copyleaks.com/v3/scans/submit/file/dar'+doc1[1:5].lower() 
+    url2='https://yoursite.com/webhook/{STATUS}/dar'+doc1[1:5].lower()
+    myobj = json.dumps({'base64':str(dats)[2:-1],'filename':'file.pdf','properties':{"action":0,'webhooks':{'status':url2}}})  
+    response = requests.put(url , headers=headers, data=myobj)
+    print(response)
+    return redirect('https://api.copyleaks.com/dashboard/scans')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port="8081")
